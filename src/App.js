6fe1a1fa5fbf920634a1dea0a30269d9e4896d86
Steven <svramirez20@gmail.com>
2021-10-12 06:10:40 -0400

    readme update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+const [users, setUsers] = useState([]);
+const [todos, setTodos] = useState([]);
+const [currentUser, setCurrentUser] = useState("");
+const userpage = "http://jsonplaceholder.typicode.com/users"
+const todopage = "http://jsonplaceholder.typicode.com/todos"
+
+useEffect(() => {
+  
+  async function loadUsers() {
+    const response = await fetch(userpage);
+    const usersFromAPI = await response.json();
+    setUsers(usersFromAPI);
+  }
+  loadUsers();
+}, [])
+
+useEffect(() => {
+  async function loadTodos() {
+    const response = await fetch(todopage);
+    const todosFromAPI = await response.json();
+    setTodos(todosFromAPI);
+  }
+  loadTodos();
+}, [currentUser])
+
+
+console.log(users, todos)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user) => (
+        <div>
+        <h3>{user.name}</h3>
+        <button onClick={() => setCurrentUser(user.name)}>See Details</button>
+        <button onClick={() => setCurrentUser(user.id)}>See To-Dos</button>
+        
+        {currentUser === user.name && <div className="details">
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p>
+        </div>
+        }
+        </div>
+      ))}
+       
+         
     </div>
   );
 }
-
+//<img src={logo} className="App-logo" alt="logo" />
 export default App;
